48936b5dd647535cbdd3645ff1cddc0123048a0f
kubota-kohei <kubota-kohei@coworkplace.jp>
2018-06-08 16:34:10 +0900

    add header

---
+++ js/function.js
@@ -0,0 +1,206 @@
+
+//メニュー
+$("#menuBtn").click(function(){
+	  if($("#menu").hasClass("open")){
+		  $("#menu").removeClass("open");
+		  $("#menu").addClass("close");
+	  }
+	  else if($("#menu").hasClass("close")){
+		  $("#menu").removeClass("close");
+		  $("#menu").addClass("open");
+	  }
+});
+
+$("#menuClose").click(function(){
+	  if($("#menu").hasClass("open")){
+		  $("#menu").removeClass("open");
+		  $("#menu").addClass("close");
+	  }
+});
+
+$("#menu li a").click(function(){
+	  if($("#menu").hasClass("open")){
+		  $("#menu").removeClass("open");
+		  $("#menu").addClass("close");
+	  }
+});
+
+//メンバー
+
+	$("#member01").click(function(){
+	  if($("#memberDetail01").hasClass("open")){
+		  $("#memberDetail01").removeClass("open");
+		  $("#memberDetail01").addClass("close");
+	  }
+	  else if($("#memberDetail01").hasClass("close")){
+		  $("#memberDetail01").removeClass("close");
+		  $("#memberDetail01").addClass("open");
+	  }
+	});
+	$("#closeBtn01").click(function(){
+	  if($("#memberDetail01").hasClass("open")){
+		  $("#memberDetail01").removeClass("open");
+		  $("#memberDetail01").addClass("close");
+	  }
+	  else if($("#memberDetail01").hasClass("close")){
+		  $("#memberDetail01").removeClass("close");
+		  $("#memberDetail01").addClass("open");
+	  }
+	});
+	
+	//member02
+	$("#member02").click(function(){
+	  if($("#memberDetail02").hasClass("open")){
+		  $("#memberDetail02").removeClass("open");
+		  $("#memberDetail02").addClass("close");
+	  }
+	  else if($("#memberDetail02").hasClass("close")){
+		  $("#memberDetail02").removeClass("close");
+		  $("#memberDetail02").addClass("open");
+	  }
+	});
+	$("#closeBtn02").click(function(){
+	  if($("#memberDetail02").hasClass("open")){
+		  $("#memberDetail02").removeClass("open");
+		  $("#memberDetail02").addClass("close");
+	  }
+	  else if($("#memberDetail02").hasClass("close")){
+		  $("#memberDetail02").removeClass("close");
+		  $("#memberDetail02").addClass("open");
+	  }
+	});
+
+		//member03
+	$("#member03").click(function(){
+	  if($("#memberDetail03").hasClass("open")){
+		  $("#memberDetail03").removeClass("open");
+		  $("#memberDetail03").addClass("close");
+	  }
+	  else if($("#memberDetail03").hasClass("close")){
+		  $("#memberDetail03").removeClass("close");
+		  $("#memberDetail03").addClass("open");
+	  }
+	});
+	$("#closeBtn03").click(function(){
+	  if($("#memberDetail03").hasClass("open")){
+		  $("#memberDetail03").removeClass("open");
+		  $("#memberDetail03").addClass("close");
+	  }
+	  else if($("#memberDetail03").hasClass("close")){
+		  $("#memberDetail03").removeClass("close");
+		  $("#memberDetail03").addClass("open");
+	  }
+	});
+
+		//member04
+	$("#member04").click(function(){
+	  if($("#memberDetail04").hasClass("open")){
+		  $("#memberDetail04").removeClass("open");
+		  $("#memberDetail04").addClass("close");
+	  }
+	  else if($("#memberDetail04").hasClass("close")){
+		  $("#memberDetail04").removeClass("close");
+		  $("#memberDetail04").addClass("open");
+	  }
+	});
+	$("#closeBtn04").click(function(){
+	  if($("#memberDetail04").hasClass("open")){
+		  $("#memberDetail04").removeClass("open");
+		  $("#memberDetail04").addClass("close");
+	  }
+	  else if($("#memberDetail04").hasClass("close")){
+		  $("#memberDetail04").removeClass("close");
+		  $("#memberDetail04").addClass("open");
+	  }
+	});
+	
+		//member05
+	$("#member05").click(function(){
+	  if($("#memberDetail05").hasClass("open")){
+		  $("#memberDetail05").removeClass("open");
+		  $("#memberDetail05").addClass("close");
+	  }
+	  else if($("#memberDetail05").hasClass("close")){
+		  $("#memberDetail05").removeClass("close");
+		  $("#memberDetail05").addClass("open");
+	  }
+	});
+	$("#closeBtn05").click(function(){
+	  if($("#memberDetail05").hasClass("open")){
+		  $("#memberDetail05").removeClass("open");
+		  $("#memberDetail05").addClass("close");
+	  }
+	  else if($("#memberDetail05").hasClass("close")){
+		  $("#memberDetail05").removeClass("close");
+		  $("#memberDetail05").addClass("open");
+	  }
+	});
+	
+		//member06
+	$("#member06").click(function(){
+	  if($("#memberDetail06").hasClass("open")){
+		  $("#memberDetail06").removeClass("open");
+		  $("#memberDetail06").addClass("close");
+	  }
+	  else if($("#memberDetail06").hasClass("close")){
+		  $("#memberDetail06").removeClass("close");
+		  $("#memberDetail06").addClass("open");
+	  }
+	});
+	$("#closeBtn06").click(function(){
+	  if($("#memberDetail06").hasClass("open")){
+		  $("#memberDetail06").removeClass("open");
+		  $("#memberDetail06").addClass("close");
+	  }
+	  else if($("#memberDetail06").hasClass("close")){
+		  $("#memberDetail06").removeClass("close");
+		  $("#memberDetail06").addClass("open");
+	  }
+	});
+	
+	
+		//member07
+	$("#member07").click(function(){
+	  if($("#memberDetail07").hasClass("open")){
+		  $("#memberDetail07").removeClass("open");
+		  $("#memberDetail07").addClass("close");
+	  }
+	  else if($("#memberDetail07").hasClass("close")){
+		  $("#memberDetail07").removeClass("close");
+		  $("#memberDetail07").addClass("open");
+	  }
+	});
+	$("#closeBtn07").click(function(){
+	  if($("#memberDetail07").hasClass("open")){
+		  $("#memberDetail07").removeClass("open");
+		  $("#memberDetail07").addClass("close");
+	  }
+	  else if($("#memberDetail07").hasClass("close")){
+		  $("#memberDetail07").removeClass("close");
+		  $("#memberDetail07").addClass("open");
+	  }
+	});
+	
+	
+		//member08
+	$("#member08").click(function(){
+	  if($("#memberDetail08").hasClass("open")){
+		  $("#memberDetail08").removeClass("open");
+		  $("#memberDetail08").addClass("close");
+	  }
+	  else if($("#memberDetail08").hasClass("close")){
+		  $("#memberDetail08").removeClass("close");
+		  $("#memberDetail08").addClass("open");
+	  }
+	});
+	$("#closeBtn08").click(function(){
+	  if($("#memberDetail08").hasClass("open")){
+		  $("#memberDetail08").removeClass("open");
+		  $("#memberDetail08").addClass("close");
+	  }
+	  else if($("#memberDetail08").hasClass("close")){
+		  $("#memberDetail08").removeClass("close");
+		  $("#memberDetail08").addClass("open");
+	  }
+	});
+	
